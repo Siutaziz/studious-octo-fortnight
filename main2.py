c4738c0d6be9b3428752475c95f3b5662a2df5de
Aziz <firuzfiruz646@gmail.com>
2022-11-10 15:11:27 +0500

    FIXPRICE

---
+++ main2.py
@@ -0,0 +1,32 @@
+import os 
+from datetime import datetime
+from telegram.ext import Updater , CommandHandler,Filters,MessageHandler
+now = datetime.now()
+strNow = now.strftime("%d%m%Y %H:%M:%S")
+
+
+def start(update,context):
+    update.message.reply_text("😎Wassup")
+def time(update,context):
+    update.message.reply_text('Time is'+strNow)
+
+def echo(update,context):
+    update.message.reply_text(update.message.text)
+
+def main():
+    TOKEN = '5749948322:AAGS3kgOb9aslWWCo-NTVQSSnsmHLWlLuXo'
+    updater = Updater(TOKEN,use_context=True)
+    dp = updater.dispatcher
+    
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('time', time))
+    dp.add_handler(MessageHandler(Filters.text,echo))
+
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
+
